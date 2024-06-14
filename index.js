6fd30e42b9101715086c9e7e6bc18ff43fff9983
Yared Kassa <40439273+yaredkassa@users.noreply.github.com>
2024-06-14 04:16:08 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
 import fs from "fs";
 import express from "express";
+import methodOverride from "method-override";
 import { v4 as uuidv4 } from "uuid";
-import { engine } from "express-handlebars";
+import { create } from "express-handlebars";
 
 const app = express();
-const port = 3000;
+app.use(methodOverride("_method"));
 
-app.engine("handlebars", engine());
+const port = 3000;
+const hbs = create({
+  /* config */
+  helpers: {
+    importance(value) {
+      let icon = '';
+      for (let index = 0; index <= value; index++) {
+        if (index>0) {
+          icon = icon + "♥";
+        }         
+      }
+      return icon;
+    },
+  },
+});
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", "source/public/view");
 
-// app.use(express.static(__dirname + "/source/public"));
 app.use(express.static("source/public"));
 
-const getTasks = () => {
+const getTasks = (task_flag) => {
   const jsonData = fs.readFileSync("source/public/model/todo.json");
-  return JSON.parse(jsonData);
+  let tasks = JSON.parse(jsonData);
+  
+  if(task_flag=='*'){
+    return tasks;
+  }else{
+      let task = tasks.filter((e) => e.id == task_flag);
+      return task;
+  }
 };
+
+function updateObject(update) {
+
+  for (var i = 0; i < update.jsonRS.length; i++) {
+    if (
+      update.jsonRS[i][update.lookupField] === update.lookupKey ||
+      update.lookupKey === "*"
+    ) {
+      update.jsonRS[i][update.targetField] = update.targetData;
+      if (!update.checkAllRows) {
+        return;
+      }
+    }
+  }
+}
 
 /**
  *  UI routes
@@ -25,24 +63,39 @@ const getTasks = () => {
 app.get("/", (req, res) => {
   res.render("home", {
     layout: false,
-    tasks: getTasks(),
+    tasks: getTasks('*'),
   });
-  // res.render("home");
+});
+
+app.get("/show/:id", (req, res) => {
+  res.render("home", {
+    layout: false,
+    tasks: getTasks(req.params.id),
+  });
 });
 
 app.get("/form", (req, res) => {
+  let detail = [{
+  edit : false,
+  }];
+
   res.render("form", {
     layout: false,
-    tasks: '',
+    task: detail,
   });
-  // res.render("form");
 });
 
 app.get("/edit/:id", function (req, res) {
+  
+    let task_detail = JSON.parse(JSON.stringify(getTasks(req.params.id)));
+    task_detail[0].edit = true;
+
   res.render("form", {
     layout: false,
-    task: req.params.id,
+    edit: true,
+    task: task_detail,
   });
+
 });
 
 
@@ -71,15 +124,9 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/api/", function (req, res) {
   fs.readFile("source/public/model/todo.json", "utf8", function (err, data) {
     let tasks = JSON.parse(data);
-
     let new_task = JSON.parse(JSON.stringify(req.body));
-
-    new_task.id = uuidv4();
-
-    if (req.body.state == null) {
-      new_task.state = "closed";
-    }
     
+    new_task.id = uuidv4();
     tasks.push(new_task);
     
     fs.writeFile(
@@ -92,71 +139,54 @@ app.post("/api/", function (req, res) {
       },
       (err) => {
         if (err) console.log(err);
-        else console.log('success');
       }
     );
-    // res.sendStatus(200);
+      
+    let detail = [
+      {
+        edit: true,
+        duedate: new_task.duedate,
+        important: new_task.important,
+        completed: Boolean(new_task.completed),
+        title: new_task.title,
+        description: new_task.description,
+        id: new_task.id,
+      },
+    ];
 
     res.render("form", {
       layout: false,
-      tasks: 'old',
-      id: new_task.id,
+      task: detail,
     });
 
-
   });
 
 });
 
 
-app.delete("/api/:id", function (req, res) {
-  fs.readFile("source/public/model/todo.json", "utf8", function (err, data) {
-    data = JSON.parse(data);
-    var id = "user" + req.params.id;
-    var user = data[id];
-    delete data["user" + req.params.id];
-    res.end(JSON.stringify(data));
-  });
-});
 
 app.put("/api/:id", function (req, res) {
-
-  // function setVal(update) {
-
-  //   for (var i = 0; i < update.jsonRS.length; i++) {
-  //     if (
-  //       update.jsonRS[i][update.lookupField] === update.lookupKey ||
-  //       update.lookupKey === "*"
-  //     ) {
-  //       update.jsonRS[i][update.targetField] = update.targetData;
-  //       if (!update.checkAllRows) {
-  //         return;
-  //       }
-  //     }
-  //   }
-  // }
-
   // var jsonObj = [
   //   { Id: "1", Username: "Ray", FatherName: "Thompson" },
   //   { Id: "2", Username: "Steve", FatherName: "Johnson" },
   //   { Id: "3", Username: "Albert", FatherName: "Einstein" },
   // ];
-  //          var update = {
-  //            jsonRS: jsonObj,
-  //            lookupField: "Id",
-  //            lookupKey: 2,
-  //            targetField: "Username",
-  //            targetData: "Thomas",
-  //            checkAllRows: false,
-  //          };
-
-  //          setVal(update);
 
   fs.readFile("source/public/model/todo.json", "utf8", function (err, data) {
-    var users = JSON.parse(data);
-    var id = "user" + req.params.id;
+    let tasks = JSON.parse(data);
+    let id = req.params.id;
 
-    users[id] = req.body;
+    let update = {
+      jsonRS: jsonObj,
+      lookupField: "id",
+      lookupKey: id,
+      targetField: "Username",
+      targetData: "Thomas",
+      checkAllRows: false,
+    };
+
+    updateObject(update);
+
     res.end(JSON.stringify(users));
   });
 });
--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,66 +1,66 @@
-// util functions
-const saveTasks = (data) => {
-  const stringifyData = JSON.stringify(data);
-  // fs.writeFileSync("source/public/model/todo.json", stringifyData);
+// // util functions
+// const saveTasks = (data) => {
+//   const stringifyData = JSON.stringify(data);
+//   // fs.writeFileSync("source/public/model/todo.json", stringifyData);
 
-};
+// };
 
-// Get the button that opens the modal
-let button_x = document.getElementById("toggle_theme");
+// // Get the button that opens the modal
+// let button_x = document.getElementById("toggle_theme");
 
-if (button_x) {
-  // When the user clicks the button, open the modal
-  button_x.onclick = () => {
-    console.log("Good Job Yared!");
-  };
-}
-
-fetch("model/todo.json").then((results) => results.json().then(console.log));
-
-var ele = document.getElementById("taskForm");
-if(ele.addEventListener){
-    ele.addEventListener("submit", callback, false);  //Modern browsers
-}else if(ele.attachEvent){
-    ele.attachEvent('onsubmit', callback);//Old IE
-}
-
-document.querySelector("#taskForm").addEventListener("submit", function (e) {
-  if (!isValid) {
-    e.preventDefault(); //stop form from submitting
-    console.log('THe form is submitted');
-  }
-});
-
-// async function loadNames() {
-//   const response = await fetch("model/todo.json");
-//   const json = await response.json();
-
-//   console.log("Here are the tskes", JSON.stringify(json));
+// if (button_x) {
+//   // When the user clicks the button, open the modal
+//   button_x.onclick = () => {
+//     console.log("Good Job Yared!");
+//   };
 // }
 
-// loadNames();
+// fetch("model/todo.json").then((results) => results.json().then(console.log));
 
-// Get the button that opens the modal
-// let button_create = document.getElementById("create");
+// var ele = document.getElementById("taskForm");
+// if(ele.addEventListener){
+//     ele.addEventListener("submit", callback, false);  //Modern browsers
+// }else if(ele.attachEvent){
+//     ele.attachEvent('onsubmit', callback);//Old IE
+// }
 
-// let taskForm = document.getElementById("taskForm");
-
-// if (taskForm) {
-// taskForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   let title = document.getElementById("title");
-//   let important = document.getElementById("important");
-//   let input_due_date = document.getElementById("duedate");
-//   let input_completed = document.getElementById("completed");
-//   let input_description = document.getElementById("description");
-
-//   saveTasks({
-//     title: title,
-//     day: important,
-//     open: input_due_date,
-//     task: input_completed,
-//     description: input_description,
-//   });
+// document.querySelector("#taskForm").addEventListener("submit", function (e) {
+//   if (!isValid) {
+//     e.preventDefault(); //stop form from submitting
+//     console.log('THe form is submitted');
+//   }
 // });
-// }
+
+// // async function loadNames() {
+// //   const response = await fetch("model/todo.json");
+// //   const json = await response.json();
+
+// //   console.log("Here are the tskes", JSON.stringify(json));
+// // }
+
+// // loadNames();
+
+// // Get the button that opens the modal
+// // let button_create = document.getElementById("create");
+
+// // let taskForm = document.getElementById("taskForm");
+
+// // if (taskForm) {
+// // taskForm.addEventListener("submit", (e) => {
+// //   e.preventDefault();
+
+// //   let title = document.getElementById("title");
+// //   let important = document.getElementById("important");
+// //   let input_due_date = document.getElementById("duedate");
+// //   let input_completed = document.getElementById("completed");
+// //   let input_description = document.getElementById("description");
+
+// //   saveTasks({
+// //     title: title,
+// //     day: important,
+// //     open: input_due_date,
+// //     task: input_completed,
+// //     description: input_description,
+// //   });
+// // });
+// // }
